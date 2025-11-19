5fa8854f0bba5ec395456f538234ba16055162d8
maxwmoir@gmail.com <maxwmoir@gmail.com>
2025-11-19 21:22:34 +1300

    Finished 2.1

--- my_files/C02_Basics/S01_Calculating.lean
+++ my_files/C02_Basics/S01_Calculating.lean
@@ -113,10 +113,24 @@ section
 variable (a b c d : ℝ)
 
 example : (a + b) * (c + d) = a * c + a * d + b * c + b * d := by
-  sorry
+  rw [mul_add, add_mul, add_mul, ←add_assoc]
+  rw [add_assoc (a * c), add_comm (b * c), ←add_assoc]
 
-example (a b : ℝ) : (a + b) * (a - b) = a ^ 2 - b ^ 2 := by
-  sorry
+example : (a + b) * (c + d) = a * c + a * d + b * c + b * d :=
+  calc
+    (a + b) * (c + d) = a*c + b*c + a*d + b*d  := by
+      rw [mul_add, add_mul, add_mul, ←add_assoc]
+    _= a * c + a * d + b * c + b * d := by
+      rw [add_assoc (a * c), add_comm (b * c), ←add_assoc]
+
+example (a b : ℝ) : (a + b) * (a - b) = a ^ 2 - b ^ 2 :=
+
+  calc
+    (a + b) * (a - b) = (a * a) - (b * b) := by
+      rw [mul_sub, add_mul, add_mul, ←sub_sub, mul_comm b, ←add_sub, sub_self, add_zero]
+    _ = a ^ 2 - b ^ 2 := by
+      rw [pow_two, pow_two]
+
 
 #check pow_two a
 #check mul_sub a b c
